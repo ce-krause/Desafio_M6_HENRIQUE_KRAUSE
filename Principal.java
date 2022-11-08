ea6091bb76d1129490aeb496d800a9860e5814a2
Henrique Krause <ce.henriquekrause@gmail.com>
2022-11-08 15:21:52 -0300

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -167,6 +167,7 @@ public class Principal {
             }
 
             mec.imprimeUniversidades(universidades);
+            mec.maisCara(universidades);
             mec.universidadesDoSul(universidades);
 
         }
--- construtores/MEC.java
+++ construtores/MEC.java
@@ -1,6 +1,7 @@
 package construtores;
 
 import universidade.Universidade;
+import universidade.filhas.Privada;
 import universidade.filhas.Publica;
 
 public class MEC extends ConstrutorUniversidade {
@@ -24,13 +25,41 @@ public class MEC extends ConstrutorUniversidade {
 
     }
 
+    public void maisCara(Universidade[] universidades) {
+
+        Privada universidadePrivada = null;
+
+        double mensalidade = 0;
+
+        System.out.println("\nImprimindo dados da universidade privada mais cara instanciada:\n");
+
+        for (Universidade universidade : universidades) {
+
+            if (universidade.toString().contains("Universidade Privada")) {
+
+                universidadePrivada = (Privada) universidade;
+
+                if (universidadePrivada.getValorMensalidade() > mensalidade) {
+
+                    mensalidade = universidadePrivada.getValorMensalidade();
+
+                }
+
+            }
+
+        }
+
+        universidadePrivada.imprimeInfo();
+
+    }
+
     public void universidadesDoSul(Universidade[] universidades) {
 
         System.out.println("\nImprimindo dados das universidades públicas da região Sul instanciadas:\n");
 
         for (Universidade universidade : universidades) {
 
-            if (universidade.toString() == "Universidade Pública") {
+            if (universidade.toString().contains("Universidade Pública")) {
 
                 Publica universidadePublica = (Publica) universidade;
 
